eed740ccf3c93d5554287f2ca00dc1ed35273e26
Mikhail Bakulin <mibak@yandex.ru>
2024-09-05 21:49:51 +0300

    Add: Add User entity and UserRepository.

--- src/main/java/main/cloudfilestorage/model/User.java
+++ src/main/java/main/cloudfilestorage/model/User.java
@@ -1,5 +1,6 @@
 package main.cloudfilestorage.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class User {
     @Id
     private Long id;
 
+    @Column(name = "user-name")
     private String userName;
 
     private String password;

e9827f750c257042e9c47eb46cc1c17f0c5aeee6
Mikhail Bakulin <mibak@yandex.ru>
2024-09-18 16:27:11 +0400

    Add: Add the securiryFilterChain configuration method for login.

--- src/main/java/main/cloudfilestorage/config/AppConfiguration.java
+++ src/main/java/main/cloudfilestorage/config/AppConfiguration.java
@@ -1,7 +1,22 @@
 package main.cloudfilestorage.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class AppConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/files","/search").permitAll())
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll())
+                .build();
+    }
 }

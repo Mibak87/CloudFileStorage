4cdff985324eb97f8b6c8ba82e7bb1a64be05a7d
Mikhail Bakulin <mibak@yandex.ru>
2024-09-30 22:59:29 +0300

    Feat: Add files endpoint.

--- src/main/java/main/cloudfilestorage/controller/MainController.java
+++ src/main/java/main/cloudfilestorage/controller/MainController.java
@@ -1,12 +1,48 @@
 package main.cloudfilestorage.controller;
 
+import main.cloudfilestorage.model.User;
+import main.cloudfilestorage.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String userName, @RequestParam String password) {
+
+        return "redirect:/files";
+    }
+
+    @RequestMapping("/registration")
+    public String register() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String processLogin(@RequestParam String userName, @RequestParam String password,
+                               @RequestParam String passwordRepeat) {
+        if (password.equals(passwordRepeat)) {
+            User user = new User(userName,password);
+            userService.register(user);
+            return "redirect:/files";
+        }
+        return "redirect:/registration";
+    }
+
+    @RequestMapping("/files")
+    public String files() {
+        return "files";
     }
 }

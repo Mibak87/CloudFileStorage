25a4223462b39ad79231bb11d1533ebe5bee9329
Mikhail Bakulin <mibak@yandex.ru>
2024-09-30 22:58:49 +0300

    Feat: Add annotation for generation of id.

--- src/main/java/main/cloudfilestorage/model/User.java
+++ src/main/java/main/cloudfilestorage/model/User.java
@@ -1,21 +1,29 @@
 package main.cloudfilestorage.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@Table(name = "app-user")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "user-name")
     private String userName;
 
     private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 }

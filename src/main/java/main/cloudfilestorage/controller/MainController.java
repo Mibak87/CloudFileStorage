deaba5ca679e875b57438143bd68fc6e30007d9f
Mikhail Bakulin <mibak@yandex.ru>
2024-10-13 22:42:09 +0300

    Feat: Add search controller. Add search form to the files.html. Add search.html.

--- src/main/java/main/cloudfilestorage/controller/MainController.java
+++ src/main/java/main/cloudfilestorage/controller/MainController.java
@@ -5,11 +5,12 @@ import main.cloudfilestorage.dto.RegisterDto;
 import main.cloudfilestorage.model.User;
 import main.cloudfilestorage.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -43,12 +44,31 @@ public class MainController {
             User user = new User(registerDto.getUserName(),registerDto.getPassword());
             userService.register(user);
             return "redirect:/login";
-        //}
+       //}
         //return "redirect:/registration";
     }
 
-    @RequestMapping("/files")
-    public String files() {
+    @RequestMapping("/")
+    public String files(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("username", username);
+        model.addAttribute("query");
         return "files";
+    }
+
+    @PostMapping("/search")
+    public String postSearch(@ModelAttribute("query") String query, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("query", query);
+        return "redirect:/search";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam(required = false) String query, Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        model.addAttribute("username", username);
+        model.addAttribute("query", query);
+        return "search";
     }
 }

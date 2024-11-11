36be6eb26eeabc1d1ee03d8e361329b9363cdf78
Mikhail Bakulin <mibak@yandex.ru>
2024-11-11 16:49:57 +0300

    Fix: Fixed folder navigation.

--- src/main/java/main/cloudfilestorage/dto/ViewFilesDto.java
+++ src/main/java/main/cloudfilestorage/dto/ViewFilesDto.java
@@ -3,11 +3,13 @@ package main.cloudfilestorage.dto;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class ViewFilesDto {
     //private List<String> path;
     private String path;
     private List<String> directories;
+    private Map<String,String> allPath;
     private List<String> files;
 }
--- src/main/java/main/cloudfilestorage/service/MinioService.java
+++ src/main/java/main/cloudfilestorage/service/MinioService.java
@@ -62,16 +62,18 @@ public class MinioService {
     public ViewFilesDto getUserFiles(String userName, String path) {
         ViewFilesDto viewFilesDto = new ViewFilesDto();
         String userDirectory = getUserDirectory(userName);
-        if (path != null) {
-            userDirectory = userDirectory + path;
+        if (path == null) {
+            path = "";
+            //userDirectory = userDirectory + path;
         }
+        userDirectory = userDirectory + path;
         log.info("Получаем все файлы пользователя " + userName + " из папки " + userDirectory);
         List<String> allUserFiles = minioRepository.getAllFilesByUser(userDirectory);
         log.info("Вот они: " + allUserFiles);
         List<String> userFiles = new ArrayList<>();
         List<String> userDirectories = new ArrayList<>();
         //List<String> userPath = new ArrayList<>();
-        viewFilesDto.setPath(userDirectory);
+        viewFilesDto.setPath(path);
         for (String userFile : allUserFiles) {
             if (userFile.equals(userDirectory)) {
                 continue;

0a2a41fd3329aa21adf48e2ad50982a3d43c7164
Mikhail Bakulin <mibak@yandex.ru>
2024-12-10 15:37:43 +0300

    Feat: Add testing of the registration new user by the testconteiners.

---
+++ src/test/java/main/cloudfilestorage/RegistrationTest.java
@@ -0,0 +1,67 @@
+package main.cloudfilestorage;
+
+import main.cloudfilestorage.exception.UniqueUserNameException;
+import main.cloudfilestorage.model.User;
+import main.cloudfilestorage.repository.UserRepository;
+import main.cloudfilestorage.service.UserService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
+public class RegistrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:latest"
+    );
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void registerNewUser() {
+        User newUser = new User("aaa","123");
+        try {
+            userService.register(newUser);
+        } catch (UniqueUserNameException e) {
+
+        }
+       assertEquals("aaa",userRepository.findByUsername("aaa").get().getUsername());
+
+    }
+}

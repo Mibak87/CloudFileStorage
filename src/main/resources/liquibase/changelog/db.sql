d311c2de11149c0a38661070994d06c424628779
Mikhail Bakulin <mibak@yandex.ru>
2024-12-12 22:30:29 +0300

    Fix: Fixed the bug of the liquibase create table.

--- src/main/resources/liquibase/changelog/db.sql
+++ src/main/resources/liquibase/changelog/db.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255),
-    role VARCHAR(255),
-    PRIMARY KEY (id)
+    role VARCHAR(255)
     );
